a0608ee5a1cc57c08d3564372b4ae1d1cf29666d
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2015-06-02 22:20:50 +0200

    backup commit 2015.06.02 22:20

--- src/lib/numbo/detail/ppfun_1d_nu.hpp
+++ src/lib/numbo/detail/ppfun_1d_nu.hpp
@@ -17,7 +17,7 @@
 namespace Numbo { namespace Detail {
 /** \ingroup NumboPPFun_1d_nu
  * @{ */
-/** // doc: ppfun_1d_nu_ppform_eval() {{{
+/** // doc: ppfun_1d_nu_ppform_eval_aux() {{{
  * \brief Evaluate *j*-th derivative of pp function 
  *
  * This function returns value of *j*-th derivative \f$ D^j f(x),\; j \ge 0 \f$
@@ -27,11 +27,11 @@ namespace Numbo { namespace Detail {
  * The original ppform is defined by matrix of derivatives
  * \f$ \mathbf{C} = (C_{j,i}) = D^j f(\xi_i^{+}),\; j = 0,\dots,k-1,\;\;
  * i = 0, \dots, l > 0\f$. This function uses only the *i*-th column of the
- * matrix \f$\mathbf{C}\f$, that is it assumes that <tt>c[m] = \f$C_{m,i},\;\;
+ * matrix \f$\mathbf{C}\f$, that is it assumes that <tt>c[s*m] = \f$C_{m,i},\;\;
  * m = 0, \dots, k-1\f$</tt>.
  *
  * The function returns the evaluated derivative computed as the following sum
- * \f[ D^j f(x) = \sum_{m=1}^{k-1} \mathtt{c[m]} \cdot \mathtt{h}^{m-j}/(m-j)!.\f]
+ * \f[ D^j f(x) = \sum_{m=1}^{k-1} \mathtt{c[s*m]} \cdot \mathtt{h}^{m-j}/(m-j)!.\f]
  * Internally it uses the same recursive formula as the de Boor's function 
  * <a href="http://pages.cs.wisc.edu/~deboor/pgs/ppvalu.f">ppvalu()</a> [1].
  *
@@ -39,13 +39,16 @@ namespace Numbo { namespace Detail {
  *    order of the piecewise polynomial used to represent the pp function;
  *    it must be *k* > 0,
  * \param[in] j
- *    order of the derivative to be evaluated; it must hold *j >= 0*;
+ *    order of the derivative to be evaluated; it must hold *j* >= 0;
  *    for *j* = 0 the function value is evaluated; *j* >= k yields zero,
  * \param[in] c
- *    random access iterator pointing to the beginning of the *i*-th row of
+ *    random access iterator pointing to the beginning of the *i*-th column of
  *    coefficient matrix \f$\mathbf{C} = (C_{ji}) := D^{j-1}f(\xi_i^{+})\f$
- *    (the ppform of \f$f\f$); it is assumed that <tt>c[m] = \f$C_{m,i},\;\;
+ *    (the ppform of \f$f\f$); it is assumed that <tt>c[s*m] = \f$C_{m,i},\;\;
  *    m = 0, \dots, k-1\f$</tt>,
+ * \param[in] s
+ *    stride, determines the memory distance (expressed in number of elements)
+ *    between consecutive columns of matrix \f$\mathbf{C}\f$,
  * \param[in] h
  *    coordinate of the point at which the derivative (or value) has to be
  *    evaluated; it's assumed that \f$0 \le h < \xi_{i+1} - \xi_i\f$, for some
@@ -60,16 +63,56 @@ namespace Numbo { namespace Detail {
  * 1. de Boor, Carl. A practical Guide to Splines. Springer 2001. ISBN
  *    0-387-95366-3.
  */ // }}}
-template<typename CIter, typename T>
+template<typename CIter, typename H>
 typename std::iterator_traits<CIter>::value_type
-ppfun_1d_nu_ppform_eval(int k, int j, CIter c, T h) 
+ppfun_1d_nu_ppform_eval_aux(int k, int j, CIter c, int s, H h) 
 {
   typedef typename std::iterator_traits<CIter>::value_type C;
   C v = (C)0;
-  int m, d = k - j;
-  for(m = k - 1; m >= j; --m, --d)
-    v = (v/d)*h + c[m];
+  int sm, sj = s*j, d = k - j;
+  for(sm = s*(k - 1); sm >= sj; sm -= s, --d)
+    v = (v/d)*h + c[sm];
   return v;
+}
+/** // doc: pprun_1d_nu_break_search_aux() {{{
+ * \todo Write documentation
+ */ // }}}
+template<typename XIIter, typename X, typename H>
+void
+ppfun_1d_nu_break_search_aux(int nd, int id, int n, XIIIter xi, X x, H* h)
+{
+  typedef typename std::iterator_traits<CIter>::value_type XI;
+  int i;
+  XI xii;
+  for(i = id; i < n-1; i += nd)
+    {
+      xii = xi[i];                // (1) - access to xi[i]
+      if(xii <= x && x < xi[i+1]) // (2) - access ti xi[i+1]
+        *h = x - xii;
+    }
+}
+/** // doc: pprun_1d_nu_break_search_aux() {{{
+ *
+ */ // }}}
+template<typename XIIter, typename X, typename H>
+void
+ppfun_1d_nu_break_search1(int nd, int n, XIIter xi, X x, H* h)
+{
+  int id;
+  if(x < xi[0])
+    {
+      *h = -1;
+      return;
+    }
+  if(x >= x[n-1])
+    {
+      *h = n-1;
+      return;
+    }
+
+  // Run nd threads (on GPU this is performed in parallel)
+  for(id = 0; id < nd; ++id)
+    ppfun_1d_nu_break_search_aux(nd, id, n, xi, x, h);
 }
 /** @} */
 } } // end namespace Numbo::Detail
--- src/test/numbo/detail/ppfun_1d_nu.t.h
+++ src/test/numbo/detail/ppfun_1d_nu.t.h
@@ -21,45 +21,45 @@ namespace Numbo { namespace Detail { class Ppfun1dNuTestSuite; } }
 class Numbo::Detail::Ppfun1dNuTestSuite : public CxxTest::TestSuite
 {
 public:
-  /** // doc: test__ppfun_1d_nu_ppform_eval__1() {{{
-   * \brief Test ppfun_1d_nu_ppform_eval() for k = 1
+  /** // doc: test__ppfun_1d_nu_ppform_eval_aux__1() {{{
+   * \brief Test ppfun_1d_nu_ppform_eval_aux() for k = 1
    */ // }}}
-  void test__ppfun_1d_nu_ppform_eval__1( )
+  void test__ppfun_1d_nu_ppform_eval_aux__1( )
   {
     // f(x) = 2 - a constant function, Df(x) is zero
     const double c[1] = { 2.0 };
     {
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(1, 0, c, 0.5), 2.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(1, 1, c, 0.5), 0.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(1, 0, c, 1, 0.5), 2.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(1, 1, c, 1, 0.5), 0.0);
     }
     
   }
-  /** // doc: test__ppfun_1d_nu_ppform_eval__2() {{{
-   * \brief Test ppfun_1d_nu_ppform_eval() for k = 2
+  /** // doc: test__ppfun_1d_nu_ppform_eval_aux__2() {{{
+   * \brief Test ppfun_1d_nu_ppform_eval_aux() for k = 2
    */ // }}}
-  void test__ppfun_1d_nu_ppform_eval__2( )
+  void test__ppfun_1d_nu_ppform_eval_aux__2( )
   {
     //  f(x)  = 1.0 + 2.0*x;  f(0) = 1.0;
     // Df(x) = 2.0;          Df(0) = 2.0
     const double c[2] = { 1.0, 2.0 };
     {
       // x = 0.5, 
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(2, 0, c, 0.5), 2.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(2, 1, c, 0.5), 2.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(2, 2, c, 0.5), 0.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(2, 0, c, 1, 0.5), 2.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(2, 1, c, 1, 0.5), 2.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(2, 2, c, 1, 0.5), 0.0);
     }
 
     {
       // x = 1.5, 
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(2, 0, c, 1.5), 4.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(2, 1, c, 1.5), 2.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(2, 2, c, 1.5), 0.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(2, 0, c, 1, 1.5), 4.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(2, 1, c, 1, 1.5), 2.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(2, 2, c, 1, 1.5), 0.0);
     }
   }
-  /** // doc: test__ppfun_1d_nu_ppform_eval__3() {{{
-   * \brief Test ppfun_1d_nu_ppform_eval() for k = 3
+  /** // doc: test__ppfun_1d_nu_ppform_eval_aux__3() {{{
+   * \brief Test ppfun_1d_nu_ppform_eval_aux() for k = 3
    */ // }}}
-  void test__ppfun_1d_nu_ppform_eval__3( )
+  void test__ppfun_1d_nu_ppform_eval_aux__3( )
   {
     //  f(x)  = 1.0 + 2.0*x + 3.0*x^2;   f(0) = 1.0;
     // Df(x)  =       2.0   + 6.0*x;    Df(0) = 2.0
@@ -67,18 +67,18 @@ public:
     const double c[3] = { 1.0, 2.0, 6.0 };
     {
       // x = 0.5, 
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 0, c, 0.5), 2.75);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 1, c, 0.5), 5.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 2, c, 0.5), 6.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 3, c, 0.5), 0.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 0, c, 1, 0.5), 2.75);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 1, c, 1, 0.5), 5.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 2, c, 1, 0.5), 6.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 3, c, 1, 0.5), 0.0);
     }
 
     {
       // x = 1.5, 
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 0, c, 1.5), 10.75);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 1, c, 1.5), 11.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 2, c, 1.5), 6.0);
-      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval(3, 3, c, 1.5), 0.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 0, c, 1, 1.5), 10.75);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 1, c, 1, 1.5), 11.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 2, c, 1, 1.5), 6.0);
+      TS_ASSERT_EQUALS(ppfun_1d_nu_ppform_eval_aux(3, 3, c, 1, 1.5), 0.0);
     }
   }
 };
